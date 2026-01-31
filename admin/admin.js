ee95233b69754f8098f6219479734eca4864cf6b
streloss <fufypdtx2@gmail.com>
2026-02-01 02:37:20 +0900

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -10,19 +10,19 @@ const DEFAULT_MAPS = [
         id: 'dust2',
         name: 'Dust II',
         description: 'Классическая пустынная карта',
-        bannerUrl: 'https://images.unsplash.com/photo-1542751371-adc38448a05e?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80'
+        bannerUrl: 'images/dust2.jpg'
     },
     {
         id: 'mirage',
         name: 'Mirage',
         description: 'Ближневосточная городская карта',
-        bannerUrl: 'https://images.unsplash.com/photo-1511512578047-dfb367046420?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80'
+        bannerUrl: 'images/mirage.jpg'
     },
     {
         id: 'inferno',
         name: 'Inferno',
         description: 'Итальянская деревня',
-        bannerUrl: 'https://images.unsplash.com/photo-1518709268805-4e9042af2176?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80'
+        bannerUrl: 'images/inferno.jpg'
     }
 ];
 
